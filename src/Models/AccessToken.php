03e48470a31bc0ff4990b2915bd997a3abfd6451
maksat607 <kuleshov.chyngyz@gmail.com>
2024-02-08 23:59:58 +0600

    adding

--- src/Models/AccessToken.php
+++ src/Models/AccessToken.php
@@ -4,6 +4,7 @@ namespace Maksatsaparbekov\KuleshovAuth\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\User;
 
 class AccessToken extends Model
 {
