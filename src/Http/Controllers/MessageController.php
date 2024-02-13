9d58d5adafe4dabb456f0f3014911b6b8c38885b
maksat607 <kuleshov.chyngyz@gmail.com>
2024-02-13 16:02:31 +0600

    adding role to user

--- src/Http/Controllers/MessageController.php
+++ src/Http/Controllers/MessageController.php
@@ -1,5 +1,7 @@
 <?php
+
 namespace Maksatsaparbekov\KuleshovAuth\Http\Controllers;
+
 use Maksatsaparbekov\KuleshovAuth\Http\Requests\ChatRequest;
 use Maksatsaparbekov\KuleshovAuth\Http\Resources\ChatRoomResource;
 use Maksatsaparbekov\KuleshovAuth\Http\Services\ChatService;
@@ -17,23 +19,21 @@ class MessageController
     {
         $model = $request->modelInstance; // Now you can use the resolved model instance
         $validated = $request->validated();
-        try {
-            $message = $this->chatService->create(
-                $model,
-                auth()->id(),
-                $validated['content'],
-                'text'
-            );
 
-            return response()->json(['message' => 'Message created successfully', 'data' => $message], 201);
-        } catch (\Exception $e) {
-            return response()->json(['error' => 'Failed to create message', 'details' => $e->getMessage()], 500);
-        }
+        $message = $this->chatService->create(
+            $model,
+            auth()->id(),
+            $validated['content'],
+            'text'
+        );
+
+        return response()->json(['message' => 'Message created successfully', 'data' => $message], 201);
     }
+
     public function index()
     {
         $model = request()->modelInstance;
-        $chatRooms = $model->chatRooms->load('messages','messages.user');
+        $chatRooms = $model->chatRooms->load('messages', 'messages.user');
         return ChatRoomResource::collection($chatRooms);
     }
 }
