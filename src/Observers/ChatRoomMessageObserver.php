74ef6b5fa74943926e61a5b950d95765bab80be0
maksat607 <kuleshov.chyngyz@gmail.com>
2024-02-19 02:11:32 +0600

    Adding Push service

--- src/Observers/ChatRoomMessageObserver.php
+++ src/Observers/ChatRoomMessageObserver.php
@@ -11,6 +11,6 @@ class ChatRoomMessageObserver
     public function created(ChatRoomMessage $message)
     {
         $push = (new FirebasePush())->setTitle('Message created')->setBody('some One wrote message to you')->setData(["user_id" => 0]);
-        (new FirebasePushService())->sendWebNotification($push,['c2T6LMRnRR6xtQmjerd6cX:APA91bE82aNc_9iCu0Vprfj--1Z97J_1ps3CoN1M48gUqw83MAaZdAJJ99oHnysZm-nLlisDOMIy4XNLNSf2XsVBSCfB0R8A79Wc6pjf08rDtzbRjH_ECyku0Kekq4mH2qzS0RhlMlvd']);
+        (new FirebasePushService())->send($push,['c2T6LMRnRR6xtQmjerd6cX:APA91bE82aNc_9iCu0Vprfj--1Z97J_1ps3CoN1M48gUqw83MAaZdAJJ99oHnysZm-nLlisDOMIy4XNLNSf2XsVBSCfB0R8A79Wc6pjf08rDtzbRjH_ECyku0Kekq4mH2qzS0RhlMlvd']);
     }
 }
