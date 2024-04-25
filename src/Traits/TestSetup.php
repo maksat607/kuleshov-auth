5e7f559777c1868da80c5404a9679b794eca86de
maksat607 <kuleshov.chyngyz@gmail.com>
2024-04-25 21:06:24 +0600

    adding tests

---
+++ src/Traits/TestSetup.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Maksatsaparbekov\KuleshovAuth\Traits;
+use Illuminate\Support\Facades\Artisan;
+Trait TestSetup
+{
+    private $user;
+    private $tickets;
+    private $authAccessToken;
+    private $structure;
+    public function setUp(): void
+    {
+        parent::setUp();
+        Artisan::call('migrate', ['--database' => 'sqlite']);
+
+    }
+
+}
