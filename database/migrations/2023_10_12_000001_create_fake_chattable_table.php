96dc9d695870e7969b30a7983765fdbaf2ac00b5
maksat607 <kuleshov.chyngyz@gmail.com>
2024-04-25 18:55:20 +0600

    adding tests

--- database/migrations/2023_10_12_000001_create_fake_chattable_table.php
+++ database/migrations/2023_10_12_000001_create_fake_chattable_table.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateChattablesTable extends Migration
+class CreateFakeChattablesTable extends Migration
 {
     /**
      * Run the migrations.
