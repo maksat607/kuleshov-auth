9d4f28253f3d8116a8ad16e963edf261fffe7eca
maksat607 <kuleshov.chyngyz@gmail.com>
2024-04-26 20:06:28 +0600

    adding tests

--- database/factories/AccessTokenFactory.php
+++ database/factories/AccessTokenFactory.php
@@ -14,8 +14,8 @@ class AccessTokenFactory extends Factory
     {
         return [
             'user_id' => fake()->optional()->randomNumber(8), // Random user ID
-            'created_at' => fake()->dateTimeThisMonth,
-            'expired_at' => fake()->dateTimeInFuture(month: 1), // Expires in 1 month
+            'created_at' => fake()->dateTimeBetween('-1 month', 'now'),
+            'expired_at' => fake()->dateTimeBetween( 'now','1 month'),
             'token' => Str::random(60), // Generate a random token
         ];
     }
