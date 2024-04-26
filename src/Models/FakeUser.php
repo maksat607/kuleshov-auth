9761bf099459599a3a616c904594b08967eac960
maksat607 <kuleshov.chyngyz@gmail.com>
2024-04-26 10:35:22 +0600

    adding tests

--- src/Models/FakeUser.php
+++ src/Models/FakeUser.php
@@ -3,12 +3,15 @@
 namespace Maksatsaparbekov\KuleshovAuth\Models;
 
 
+use Illuminate\Auth\Authenticatable;
+use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
 use Maksatsaparbekov\KuleshovAuth\Database\Factories\FakeUserFactory;
 use Maksatsaparbekov\KuleshovAuth\Traits\AuthService;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-class FakeUser extends Model
+class FakeUser extends Model implements AuthenticatableContract
 {
+    use Authenticatable;
     use AuthService;
     use HasFactory;
     protected $table = 'fake_users';
