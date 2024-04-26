686d8eb6369f09e3167ba6b3c1efd42dc10bc7b0
maksat607 <kuleshov.chyngyz@gmail.com>
2024-04-26 20:03:45 +0600

    adding tests

--- src/Models/AccessToken.php
+++ src/Models/AccessToken.php
@@ -5,6 +5,8 @@ namespace Maksatsaparbekov\KuleshovAuth\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use App\Models\User;
+use Maksatsaparbekov\KuleshovAuth\Database\Factories\AccessTokenFactory;
+
 
 class AccessToken extends Model
 {
@@ -16,6 +18,10 @@ class AccessToken extends Model
     public function user()
     {
         return $this->belongsTo(User::class);
+    }
+    protected static function newFactory()
+    {
+        return AccessTokenFactory::new();
     }
 }
 
