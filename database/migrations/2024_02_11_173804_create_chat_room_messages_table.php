aa738cbd812753e7205c181049874684525992f9
maksat607 <kuleshov.chyngyz@gmail.com>
2024-04-26 10:30:11 +0600

    adding tests

--- database/migrations/2024_02_11_173804_create_chat_room_messages_table.php
+++ database/migrations/2024_02_11_173804_create_chat_room_messages_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
-
+use Illuminate\Support\Facades\App;
 return new class extends Migration
 {
     /**
@@ -13,8 +13,12 @@ return new class extends Migration
     {
         Schema::create('chat_room_messages', function (Blueprint $table) {
             $table->id();
+            if (App::runningUnitTests()) {
+                $table->foreignId('user_id')->constrained('fake_users')->cascadeOnDelete();
+            }else{
+                $table->foreignId('user_id')->constrained('users')->cascadeOnDelete();
+            }
             $table->foreignId('chat_room_id')->constrained('chat_rooms')->cascadeOnDelete();
-            $table->foreignId('user_id')->constrained('users')->cascadeOnDelete();
             $table->text('content')->nullable();
             $table->string('type')->nullable();
             $table->timestamps();
