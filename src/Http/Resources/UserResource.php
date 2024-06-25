4531189bd69883e7d871cc438de688df56b0d396
maksat607 <kuleshov.chyngyz@gmail.com>
2024-06-25 18:36:28 +0600

    adding my devices role index

--- src/Http/Resources/UserResource.php
+++ src/Http/Resources/UserResource.php
@@ -10,6 +10,7 @@ class UserResource extends JsonResource
     {
         return [
             'phone' => $this->phone,
+            'role' => $this->getRoleNames(),
             'firebaseTokens' => $this->whenLoaded('firebaseTokens'),
         ];
     }
