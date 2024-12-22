c1395d7ad691d910216414f9f91004f878a77f4b
maksat607 <kuleshov.chyngyz@gmail.com>
2024-12-22 13:23:23 +0600

    sorting by parameters

--- src/Http/v2/Controllers/ChatController.php
+++ src/Http/v2/Controllers/ChatController.php
@@ -312,6 +312,8 @@ class ChatController
         $perPage = request()->input("per_page", 15);
         $currentPage = request()->input("page", 1);
 
+        return 2323452345;
+
         // Step 1: Get all chat rooms (without pagination)
         if (request()->user()->hasRole(['Admin', 'Manager']) && "vinz.ru" == env('APP_NAME')) {
             Log::info('Admin or Manager');
