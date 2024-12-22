dd3460610db6cb36981926aa3bca45510ed39eef
maksat607 <kuleshov.chyngyz@gmail.com>
2024-12-22 13:17:35 +0600

    sorting by parameters

--- src/Http/v2/Controllers/ChatController.php
+++ src/Http/v2/Controllers/ChatController.php
@@ -315,7 +315,7 @@ class ChatController
         // Step 1: Get all chat rooms (without pagination)
         if (request()->user()->hasRole(['Admin', 'Manager']) && "vinz.ru" == env('APP_NAME')) {
             Log::info('Admin or Manager');
-            $allChatRooms = ChatRoom::orderByLatestMessage()->filter($filters)->get();
+            $allChatRooms = ChatRoom::filter($filters)->get();
         } else {
             Log::info('Not Admin or Manager');
             $allChatRooms = request()->user()->chatRooms()->orderByLatestMessage()->filter($filters)->get();
