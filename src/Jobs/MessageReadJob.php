9abf11f6a5927f63a2c29d1f0b2587840413871c
maksat607 <kuleshov.chyngyz@gmail.com>
2025-01-24 14:27:50 +0600

    trying to remove error

--- src/Jobs/MessageReadJob.php
+++ src/Jobs/MessageReadJob.php
@@ -16,12 +16,14 @@ class MessageReadJob implements ShouldQueue
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
     protected $userId;
-    protected $chatRoomMessage;
+    protected $model;
+    protected $type;
 
-    public function __construct($userId, $chatRoomMessage)
+    public function __construct($userId, $model)
     {
         $this->userId = $userId;
-        $this->chatRoomMessage = $chatRoomMessage;
+        $this->model = $model;
+        $this->type = class_basename($model);
     }
 
     /**
@@ -29,9 +31,19 @@ class MessageReadJob implements ShouldQueue
      */
     public function handle(): void
     {
-        DB::transaction(function () {
-            $participant = (new AddParticipantAction())->execute($this->chatRoomMessage->chatRoom, $this->userId);
-            (new MessageReadAction())->execute($this->chatRoomMessage, $participant->id);
-        });
+        if ($this->type == 'ChatRoomMessage') {
+            DB::transaction(function () {
+                $participant = (new AddParticipantAction())->execute($this->model->chatRoom, $this->userId);
+                (new MessageReadAction())->execute($this->model, $participant->id);
+            });
+        }
+        if ($this->type == 'ChatRoom') {
+            DB::transaction(function () {
+                $participant = (new AddParticipantAction())->execute($this->model, $this->userId);
+                foreach ($this->model->messages()->where('user_id', $this->userId)->get() as $chatRoomMessage) {
+                    (new MessageReadAction())->execute($chatRoomMessage, $participant->id);
+                }
+            });
+        }
     }
 }
