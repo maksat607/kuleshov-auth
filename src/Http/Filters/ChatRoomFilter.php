2f0195118b8a75fa731d094d210fbbd3325ad6a6
maksat607 <kuleshov.chyngyz@gmail.com>
2025-02-17 12:30:41 +0600

    adding priority

--- src/Http/Filters/ChatRoomFilter.php
+++ src/Http/Filters/ChatRoomFilter.php
@@ -44,7 +44,7 @@ class ChatRoomFilter
         // Check if the priority column exists in the table
         //if (Schema::hasColumn('chat_rooms', 'priority')) {
             // Add priority sorting if value is 1 or more
-            $this->builder->orderBy('priority', 'asc');
+            $this->builder->orderBy('priority', 'desc');
 //        }
     }
 
