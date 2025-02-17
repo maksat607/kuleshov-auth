0e451cb6f2399fe627c1d210c1b523fc17666e78
maksat607 <kuleshov.chyngyz@gmail.com>
2025-02-17 12:14:57 +0600

    adding priority

--- src/Models/ChatRoom.php
+++ src/Models/ChatRoom.php
@@ -62,8 +62,8 @@ class ChatRoom extends Model
     use \Awobaz\Compoships\Compoships;
     use HasFactory;
 
-    protected $appends = ['chat_room_id', 'title', 'thumbnail', 'model_id', 'model_type', 'chat_creator_id', 'chat_creator_name', 'chat_creator_phone', 'route_name', 'unread_count', 'total_count', 'read_by_manager','status_id','priority'];
-    protected $visible = ['chat_room_id', 'title', 'thumbnail', 'model_id', 'model_type', 'chat_creator_id', 'chat_creator_name', 'chat_creator_phone', 'messages', 'messages.user', 'route_name', 'unread_count', 'unread_count', 'total_count', 'read_by_manager','status_id','priority'];
+    protected $appends = ['chat_room_id', 'title', 'thumbnail', 'model_id', 'model_type', 'chat_creator_id', 'chat_creator_name', 'chat_creator_phone', 'route_name', 'unread_count', 'total_count', 'read_by_manager','status_id'];
+    protected $visible = ['chat_room_id', 'title', 'thumbnail', 'model_id', 'model_type', 'chat_creator_id', 'chat_creator_name', 'chat_creator_phone', 'messages', 'messages.user', 'route_name', 'unread_count', 'unread_count', 'total_count', 'read_by_manager','status_id'];
     protected $guarded = [];
 
     protected static function boot()
