d876643e674320a25f302dafe8932f10a456c54f
maksat607 <kuleshov.chyngyz@gmail.com>
2025-08-05 15:41:03 +0600

    adding clean phone logic

--- src/Http/Middleware/AuthenticateAccessTokenRemote.php
+++ src/Http/Middleware/AuthenticateAccessTokenRemote.php
@@ -54,7 +54,12 @@ class AuthenticateAccessTokenRemote
                 $responseData = json_decode($response->getBody() , true);
 
 //                $user = User::where('phone', $responseData['phone'])->first();
-                $user = User::whereRaw("REGEXP_REPLACE(phone, '[^0-9]', '') = ?", [numbers_only($responseData['phone'])])->first();
+                if(request()->has('email')){
+                    $user = User::where('email', $responseData['email'])->first();
+                }else{
+                    $user = User::whereRaw("REGEXP_REPLACE(phone, '[^0-9]', '') = ?", [numbers_only($responseData['phone'])])->first();
+                }
+
 
                 if (!$user)
                 {
